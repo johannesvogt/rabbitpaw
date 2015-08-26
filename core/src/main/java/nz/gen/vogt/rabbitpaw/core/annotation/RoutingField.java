af25aa4a529e70a68ee9e8a07acce2af54483ae0
Johannes Vogt <joh.vogt@gmail.com>
2015-08-27 07:19:22 +1200

    renamed annotation

---
+++ core/src/main/java/nz/gen/vogt/rabbitpaw/core/annotation/RoutingField.java
@@ -0,0 +1,15 @@
+package nz.gen.vogt.rabbitpaw.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by johannes on 15/08/15.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RoutingField {
+    String name() default "";
+}
